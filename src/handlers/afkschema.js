6daf83cc4c150f3852dd32308493e53e18a45c52
tlh <75905663+niyuki@users.noreply.github.com>
2021-10-20 21:07:24 +0200

    Delete afkschema.js

--- src/handlers/afkschema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-let afk = new mongoose.Schema({
-    guild = String,
-    user = String,
-    reason = String
-})
-
-module.exports = mongoose.model('afk', afk)
