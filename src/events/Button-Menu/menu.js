796fba3e182d7330ad9e3442f12074cc57dbc8d3
tlh <75905663+niyuki@users.noreply.github.com>
2021-10-23 14:14:47 +0200

    Update menu.js

--- src/events/Button-Menu/menu.js
+++ src/events/Button-Menu/menu.js
@@ -1,10 +1,9 @@
 const client = require('../../niyuki');
 const config = require('../../setting.json')
 const { MessageEmbed } = require('discord.js')
-
 client.on('clickMenu', async(menu) => {
     let Reason = ""
-      if(!menu.clicker.roles.cache.get(config.rolelist.ticketauthorized)) return
+      if(!menu.clicker.member.roles.cache.get(config.rolelist.ticketauthorized)) return
       if(menu.values[0] === 'r1') {
         await menu.reply.defer({ ephemeral: true })
         await menu.channel.send('@everyone'+await client.translate(`Ticket is closing soon.. Reason is: Topic is solved`))
@@ -26,14 +25,14 @@ client.on('clickMenu', async(menu) => {
     
       if(menu.values[0] === 'r4') {
         await menu.reply.defer({ ephemeral: true })
-        await menu.channel.send(`amogus \n\n 🟨🟨🟨🟨\n🟨⬜⬜🟨\n🟨🟨🟨🟨💭\n🟨🟨🟨🟨💭\n🟨🟨🟨🟨 \n🟨............🟨`)
+        await menu.channel.send(`amogus \n\n🟨🟨🟨🟨\n🟨⬜⬜🟨\n🟨🟨🟨🟨💭\n🟨🟨🟨🟨💭\n🟨🟨🟨🟨 \n🟨............🟨`)
         Reason = "amogus \n\n 🟨🟨🟨🟨\n🟨⬜⬜🟨\n🟨🟨🟨🟨💭\n🟨🟨🟨🟨💭\n🟨🟨🟨🟨 \n🟨............🟨"
       }
       setTimeout(() => {
         menu.channel.delete()
     }, 15000)    
-          let x= client.users.cache.find(x => x.id === menu.channel.name)
-      x.send(await client.translate(`Ticket Closed. Reach ${menu.clicker} for more details - \`ID: ${menu.clicker.id}\` \`Reason: ${Reason}\` `,))
-      client.channels.cache.get(config.channellist.ticketlogchannel).send(new MessageEmbed().setAuthor(x.displayName, x.avatarURL({dynamic: true})).setColor("#55ff76").setTimestamp().setFooter('Niyuki was Alone!').setTitle('Ticket Log').setDescription(`Ticket of ${x}-(${x.id}) has succesfully closed the ticket by ${menu.clicker} \n \`Reason: ${Reason}\` \n Head over to <#${config.channellist.createticketchannel}> to create a new ticket!`))
+        let x= client.users.cache.find(x => x.id === menu.channel.name)
+      if(x) x.send(await client.translate(`Ticket Closed. Reach ${menu.clicker} for more details - \`ID: ${menu.clicker.id}\` \`Reason: ${Reason}\` `))
+      client.channels.cache.get(config.channellist.ticketlogchannel).send(new MessageEmbed().setAuthor(x.displayName, x.avatarURL({dynamic: true})).setColor("#55ff76").setTimestamp().setFooter('Niyuki was Alone!').setTitle('Ticket Log').setDescription(`Ticket of ${x}-(${x.id}) has succesfully closed the ticket by ${menu.clicker.member} \n \`Reason: ${Reason}\` \n Head over to <#${config.channellist.createticketchannel}> to create a new ticket!`))
 
     })
